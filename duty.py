46eaddd31d6f028e81fb2fc17adbf12433fa613d
dreamwind1985 <liliang@ubuntu.(none)>
2014-12-02 20:47:54 +0800

    latest version

--- duty.py
+++ duty.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 #-*-coding: utf-8-*-
 import os
-import sys 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 import time
 import xlrd
-import xlwt
-import xlutils
-from xlutils.copy import copy
+import time
 import codecs
 list_eal= []
 list_noeal = []
@@ -21,7 +21,7 @@ def noela_start(start_time):
 		if(int(start_li[0]) < 9):
 			return NORMAL
 		elif(int(start_li[0]) == 9 ):
-			if(int(start_li[1] > 6)):
+			if(int(start_li[1]) > 6):
 				return LATE
 			else:
 				return NORMAL
@@ -32,9 +32,9 @@ def noela_start(start_time):
 def noela_leave(leave_time):
 	try:
 		leave_li = leave_time.split(":")
-		if(int(leave_li[0])) > 17:
+		if(int(leave_li[0]) > 17):
 			return NORMAL
-		elif(int(leave_li[0])) < 17:
+		elif(int(leave_li[0]) < 17):
 			return LEAVE_EARLY
 		else:
 			if(int(leave_li[1]) >= 25):
@@ -49,7 +49,7 @@ def ela_start(start_time):
    		start_li = start_time.split(":")
 		if(int(start_li[0]) < 10):
 			return NORMAL
-		elif(int(start_li[0]) > 10 and int(start_li[0]) < 11):
+		elif(int(start_li[0]) >= 10 and int(start_li[0]) <= 11):
 			return LATE
 		else:
 			return ABSENT
@@ -63,11 +63,12 @@ def ela(start_time, leave_time):
 		leave_min = int(leave_li[0]) * 60 + int(leave_li[1])
 		sum_min = leave_min - start_min
 		if(sum_min >= 540):
-			return NORMAL
+			return ""
 		elif(sum_min >= 480):
-			return LEAVE_EARLY
+			leave_time = 540 - sum_min 
+			return u"早退" + str(leave_time)+u"分钟"
 		else:
-		 	return ABSENT
+		 	return u"旷工"
 	except:
 	  return EXCEPTION
 def open_excel(filename):
@@ -80,9 +81,9 @@ def open_excel(filename):
 
 def get_list(filename):
 	data = open_excel(filename)
-	print"123"
+#print"123"
 	sheet1 = data.sheet_by_name("Sheet1")
-	print "234"
+#	print "234"
 	nrows = sheet1.nrows
 
 	for rownum in range(1, nrows):
@@ -106,51 +107,46 @@ def duty(filename):
 	if 1:
 		data = open_excel(filename)
 		report_sheet = data.sheet_by_name(u"考勤日报表")
-		data = xlutils.copy.copy(data)
-		report_sheet_copy = data.get_sheet(0)
 		nrows = report_sheet.nrows
 		ncols = report_sheet.ncols
-		f = codecs.open("result.csv","wb+",encoding = "gb2312")
+		f = codecs.open("result.csv","wb+")
 		for rownum in range(0, nrows):	
 			rowval = report_sheet.row_values(rownum)			
 			write_value = ''
 			value = ""
 			for colnum in range(0, ncols):
-				tmp_value = ""
 				if(type(rowval[colnum])) is unicode :
-					#tmp_value = rowval[colnum]
 					tmp_value = rowval[colnum]
-					
-				if(type(rowval[colnum])) is str:
-				 	tmp_value = rowval[colnum].decode()
+					try:
+						tmp_value = rowval[colnum].decode("utf-8")
+						tmp_value = tmp_value.encode("gb2312")
+					except:
+						tmp_value = ""	
+					f.write(tmp_value)
+				elif ((type(rowval[colnum])) is str) :
+				 	tmp_value = rowval[colnum]
+					f.write(tmp_value)
 				else:
 					tmp_value = ""	
-				#write_value =write_value + tmp_value +u","
-				f.write(tmp_value)
+					f.write(tmp_value)
 				f.write(",")
-				print rownum
-			if(rowval[4] == "A01"):
+			if(rowval[4] == "A01") :
 				if(rowval[1] in list_noeal):
 					start_value = noela_start(rowval[5])
 					leave_value = noela_leave(rowval[6])
 					value = converse_result(start_value) +" "+ converse_result(leave_value)
-				elif(rowval[1] in list_noeal):
+				elif(rowval[1] in list_eal):
 					start_value = ela_start(rowval[5])
-					leave_value = ela(rowval[6])
-					value = converse_result(start_value)+" " +converse_result( leave_value)
+					leave_value = ela(rowval[5],rowval[6])
+					value = converse_result(start_value)+" " +leave_value
 				else:
-		 			value = u"不再考勤人员表中"		 
-				#write_value = write_value+value.encode("gb2312")+","+"\n"
+		 			value = u"不在考勤人员表中"		 
+			value = value.decode("utf-8").encode("gb2312")
 			f.write(value)
 			f.write("\n")
-#report_sheet_copy.write(rownum, 10, value)
 		f.close()
-		data.save("result.xlsx")			
 		print "seccess"
-#except Exception,e:
-#		print e
-#		print "fail to do work"
-
+'''
 if __name__ == "__main__":
 	if len(sys.argv) < 3:
 		print "useage: duty.py 考勤人员.xlsx 考勤日报表.xlsx"
@@ -159,5 +155,13 @@ if __name__ == "__main__":
 	else:
 		get_list(sys.argv[1])
 		duty(sys.argv[2])
-		print list_eal
-		print list_noeal
+#print list_eal
+#print list_noeal
+'''
+if __name__ == "__main__":
+	if os.path.isfile(u"考勤人员.xlsx") and os.path.isfile(u"考勤日报表0.xlsx"):
+		get_list(u"考勤人员.xlsx")
+		duty(u"考勤日报表0.xlsx")
+	else:
+		print "考勤人员.xlsx 或者考勤日报表0.xlsx不存在"
+		time.sleep(5)	
